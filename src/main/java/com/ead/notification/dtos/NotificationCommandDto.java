022512402f5e20f7a08171a91f94ca6eb82a205a
BenjaminFernandes <benjamin.fernandes@equiplano.com.br>
2022-05-26 17:43:25 -0300

    Adicionado config para receber a msg do microservice course

---
+++ src/main/java/com/ead/notification/dtos/NotificationCommandDto.java
@@ -0,0 +1,14 @@
+package com.ead.notification.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class NotificationCommandDto {
+    private String title;
+    private String message;
+    private UUID userId;
+}
---
+++ src/main/java/com/ead/notification/services/NotificationService.java
@@ -0,0 +1,7 @@
+package com.ead.notification.services;
+
+import com.ead.notification.models.NotificationModel;
+
+public interface NotificationService {
+    public NotificationModel saveNotification(NotificationModel notificationModel);
+}
