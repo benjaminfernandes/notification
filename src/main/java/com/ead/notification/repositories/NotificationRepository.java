035bcb90482456aecf72a3982610521958752294
BenjaminFernandes <benjamin.fernandes@equiplano.com.br>
2022-05-30 13:44:04 -0300

    Adicionando endpoints para listar e atualizar notificações

--- src/main/java/com/ead/notification/repositories/NotificationRepository.java
+++ src/main/java/com/ead/notification/repositories/NotificationRepository.java
@@ -1,10 +1,17 @@
 package com.ead.notification.repositories;
 
+import com.ead.notification.enums.NotificationStatus;
 import com.ead.notification.models.NotificationModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationModel, UUID> {
+
+    public Page<NotificationModel> findAllByUserIdAndNotificationStatus(UUID userId, NotificationStatus notificationStatus, Pageable pageable);
+    public Optional<NotificationModel> findByNotificationIdAndUserId(UUID notificationId, UUID userId);
 }
